68c016cb5f49d2f9d9e8ab8903114ebeca9e3c71
Flavie Feuillet <flavie.feuillet@gmail.com>
2026-06-30 19:01:45 +0200

    remove unnecessary comments

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,43 +1,59 @@
 package com.hemebiotech.analytics;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
+/**
+ * Represents an ordered list of symptoms with their number of occurrences. This
+ * class allows adding and counting symptoms.
+ * 
+ */
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
-			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
+	public static void main(String args[]) throws IOException {
 		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		/**
+		 * Specialized list ordered on the key by default
+		 */
+		Map<String, Integer> occurrencesCount = new TreeMap<String, Integer>();
+
+		/**
+		 * Read the file
+		 */
+		try (BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"))) {
+			String line = reader.readLine();
+
+			while (line != null) {
+				// Add the line if the symptom does not exist; otherwise, increment the value of
+				// the symptom
+				occurrencesCount.merge(line, 1, Integer::sum);
+				line = reader.readLine();
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		/**
+		 * Write in the file
+		 */
+		try (BufferedWriter writer = new BufferedWriter(new FileWriter("result.out"))) {
+			occurrencesCount.forEach((key, value) -> {
+				try {
+					writer.write(key + " : " + value);
+					writer.newLine();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			});
+			writer.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
